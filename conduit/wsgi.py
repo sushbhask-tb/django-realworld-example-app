37ca6072ca97f7a51c838585e6a977edf574fd6a
echo-agent <echo-agent[bot]@users.noreply.github.com>
2025-06-11 20:10:24 +0000

    Add Treebeard SDK for django-realworld-example-app observability

--- conduit/wsgi.py
+++ conduit/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+from treebeardhq import Treebeard
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conduit.settings")
+
+# Initialize Treebeard SDK
+Treebeard.init(project_name="django-realworld-example-app")
 
 application = get_wsgi_application()
